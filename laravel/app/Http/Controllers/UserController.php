0ce751dfc1d1a685e2c3f4f87cb5da5002c8bf9b
hxperl <hxperl@gmail.com>
2017-11-13 19:00:06 +0900

    Fix : mistyping synstanx

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UserController extends Controllers
+class UserController extends Controller
 {
     public function postSignUp(Request $request)
     {
