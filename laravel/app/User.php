0f6d8ffd8ed0a63378dc0549281d9814c4ce75ff
hxperl <hxperl@gmail.com>
2017-11-24 04:54:32 +0900

    FIX : import syntax error

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use Illuminate\Contract\Auth\Authenticatiable;
+use Illuminate\Contracts\Auth\Authenticatiable;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model implements Authenticatiable
