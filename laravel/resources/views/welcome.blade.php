c751006015a0e60b644aadeab7693205b2e4c44f
hxperl <hxperl@gmail.com>
2017-11-30 05:07:50 +0900

    FIX : remove post name

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -36,10 +36,6 @@
                     <input class="form-control" type="text" name="email" id="email">
                 </div>
                 <div class="form-group">
-                    <label for="first_name">Your First Name</label>
-                    <input class="form-control" type="text" name="first_name" id="first_name">
-                </div>
-                <div class="form-group">
                     <label for="password">Your Password</label>
                     <input class="form-control" type="password" name="password" id="password">
                 </div>
