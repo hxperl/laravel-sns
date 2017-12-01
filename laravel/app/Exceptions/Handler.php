a98e20fb76adb1a9ea868dfc2f9682c40e2749dc
hxperl <hxperl@gmail.com>
2017-12-02 02:04:37 +0900

    FIX : routing error

--- laravel/app/Exceptions/Handler.php
+++ laravel/app/Exceptions/Handler.php
@@ -58,6 +58,6 @@ class Handler extends ExceptionHandler
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
 
-        return redirect()->guest('welcome');
+        return redirect()->route('home');
     }
 }
