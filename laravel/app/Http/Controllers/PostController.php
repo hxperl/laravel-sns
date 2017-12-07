41009faed8f291a4e7feabecbebc4b8b36e36ab4
hxperl <hxperl@gmail.com>
2017-12-07 22:12:30 +0900

    FIX : error syntax

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
     {
         $post = Post::where('id', $post_id)->first();
         if (Auth::user() != $post->user) {
-            return redirect->back();
+            return redirect()->back();
         }
         $post->delete();
         return redirect()->route('dashboard')->with(['message' => 'Successfully deleted!']);
